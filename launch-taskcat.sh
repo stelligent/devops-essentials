b7f315a2c6ce0dd8afe1a80b5867e250b1004583
Paul Duvall <pduvall@gmail.com>
2020-07-09 19:03:08 +0000

    upgrade taskcat

--- launch-taskcat.sh
+++ launch-taskcat.sh
@@ -2,4 +2,4 @@
 # sudo chmod +x *.sh
 # ./launch-taskcat.sh
 
-aws cloudformation create-stack --stack-name deoa-taskcat --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-body file://pipeline-taskcat.yml
+aws cloudformation create-stack --stack-name taskcat-deoa --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-body file://pipeline-taskcat.yml

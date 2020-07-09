0b26c3255bb766daf5971b39b20841312d7c64d8
Paul Duvall <pduvall@gmail.com>
2020-07-09 18:39:31 +0000

    auto delete stack

--- delete-taskcat.sh
+++ delete-taskcat.sh
@@ -1,49 +1,31 @@
 #!/bin/bash
 # sudo chmod +x *.sh
-# ./delete-taskcat.sh 
+# ./delete-TASKCAT_CODE.sh 
 
 AWS_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
+PREFIX=tCaT
+# Name used in .taskcat.yml
+PROJECT_NAME=deoa
 
-# Generated Code by TaskCat. For example: 7806754vf2r1494aa8b64d1av821418881
-TASKCAT=${1:-TBD}  
+# Generated Code by TASKCAT_CODE. For example: 7806754vf2r1494aa8b64d1av821418881
+TASKCAT_CODE=${1:-TBD}  
 # Generated App Code for Nested Stacks. For example: 2G44LSVW82291
 NESTED_APP_CODE=${2:-TBD}
 
-echo Environment: $TASKCAT
+echo Environment: $TASKCAT_CODE
 
 echo "Removing buckets previously used by this script"
-aws s3api list-buckets --query 'Buckets[?starts_with(Name, `tcat-ce`) == `true`].[Name]' --output text | xargs -I {} aws s3 rb s3://{} --force
+aws s3api list-buckets --query 'Buckets[?starts_with(Name, `tcat-deoa`) == `true`].[Name]' --output text | xargs -I {} aws s3 rb s3://{} --force
 
-echo "Deleting Lesson 8 stacks"
-aws cloudformation delete-stack --stack-name tCaT-ce-l8-$TASKCAT-$AWS_REGION
-aws cloudformation wait stack-delete-complete --stack-name tCaT-ce-l8-$TASKCAT-$AWS_REGION
-aws cloudformation delete-stack --stack-name tCaT-ce-l8-$TASKCAT
+echo "Deleting $PROJECT_NAME stacks"
 
-echo "Deleting Lesson 5 stacks"
-aws cloudformation delete-stack --stack-name tCaT-ce-l5-rest-ddb-$TASKCAT
-aws cloudformation delete-stack --stack-name tCaT-ce-l5-rest-ebs-$TASKCAT
-aws cloudformation delete-stack --stack-name tCaT-ce-l5-rest-kms-$TASKCAT
-aws cloudformation delete-stack --stack-name tCaT-ce-l5-rest-s3-$TASKCAT
-
-echo "Deleting Lesson 4 stacks"
-aws cloudformation delete-stack --stack-name tCaT-ce-l4-$TASKCAT-cloudfront
-aws cloudformation wait stack-delete-complete --stack-name tCaT-ce-l4-$TASKCAT-cloudfront
-aws cloudformation delete-stack --stack-name tCaT-ce-l4-$TASKCAT-acm
-aws cloudformation wait stack-delete-complete --stack-name tCaT-ce-l4-$TASKCAT-acm
-aws cloudformation delete-stack --stack-name tCaT-ce-l4-$TASKCAT
-
-echo "Deleting Lesson 3 stacks"
-aws cloudformation delete-stack --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION-rds
-aws cloudformation wait stack-delete-complete --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION-rds
-aws cloudformation delete-stack --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION-NestedApp-$NESTED_APP_CODE
-aws cloudformation wait stack-delete-complete --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION-NestedApp-$NESTED_APP_CODE
-aws cloudformation delete-stack --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION
-aws cloudformation wait stack-delete-complete --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION
-aws cloudformation delete-stack --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION-vpc
-aws cloudformation wait stack-delete-complete --stack-name tCaT-ce-l3-$TASKCAT-$AWS_REGION-vpc
-aws cloudformation delete-stack --stack-name tCaT-ce-l3-$TASKCAT
-
-echo "Deleting Lesson 1 stacks"
-aws cloudformation delete-stack --stack-name tCaT-ce-lesson1-pipeline-$TASKCAT
-aws cloudformation delete-stack --stack-name tCaT-ce-lesson1-automate-$TASKCAT
-aws cloudformation delete-stack --stack-name tCaT-ce-lesson1-pipeline-$TASKCAT
+# Declare an array of string with type
+declare -a StringArray=("beanstalk" "static")
+ 
+# Iterate the string array using for loop
+for val in ${StringArray[@]}; do
+   echo $val
+   echo "Deleting $PREFIX-$PROJECT_NAME-$val-$TASKCAT_CODE"
+   aws cloudformation delete-stack --stack-name $PREFIX-$PROJECT_NAME-$val-$TASKCAT_CODE
+   aws cloudformation wait stack-delete-complete --stack-name $PREFIX-$PROJECT_NAME-$val-$TASKCAT_CODE
+done
